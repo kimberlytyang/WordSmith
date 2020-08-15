7d4dcf5af1b3bc19f0aeed839c53376e64ab8469
kimberlytyang <56359888+kimberlytyang@users.noreply.github.com>
2020-08-14 23:45:42 -0700

    Removed extra includes

--- src/unit_test.cpp
+++ src/unit_test.cpp
@@ -1,7 +1,5 @@
 #include "gtest/gtest.h"
 #include "../tests/rectangle_test.hpp"
-#include "../header/rectangle.hpp"
-#include "../header/graphic.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
