f317653c4d6a7b20b7bcfc946fe50309670c3eec
ChrisYoung1048 <chrisyoung1048@gmail.com>
2020-08-14 20:27:04 -0700

    🚚 Created folders and moved files

---
+++ header/graphic.hpp
@@ -0,0 +1,13 @@
+#ifndef GRAPHIC_HPP
+#define GRAPHIC_HPP
+
+
+class Graphic {
+	public:
+		Graphic() { };
+		virtual void draw() = 0;
+		virtual void insert(Graphic* g);
+		virtual void remove(Graphic* g);
+		virtual Graphic* getChild(int i) = 0;
+};
+#endif
