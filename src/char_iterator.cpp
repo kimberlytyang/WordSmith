d957cf4ccbd7ca40875820f9375c0fd7455c22d4
kimberlytyang <kimberly.t.yang@gmail.com>
2020-08-15 20:28:01 -0700

    Checked for out of bounds with line count and fixed prompt file path

--- src/char_iterator.cpp
+++ src/char_iterator.cpp
@@ -7,25 +7,43 @@
 
 vector<char> CharIterator::loadPrompt(int num) {
 	vector<char> prompt;
-	std::ifstream inFS("../res/prompt.txt", std::ifstream::in);
+
+	ifstream inCount("res/prompt.txt");
+	if (!inCount.is_open()) {
+                cout << "Error opening prompt file to count lines." << endl;
+                prompt.push_back('!');
+                return prompt;
+        }
+
+	int lineCount = 0;
+	string hold = "";
+	while (getline(inCount, hold)) {
+		lineCount++;
+	}
+
+	inCount.close();
+
+	ifstream inFS("res/prompt.txt");
 	if (!inFS.is_open()) {
-		cout << "Error opening prompt file." << endl;
+		cout << "Error opening prompt file to extract." << endl;
 		prompt.push_back('!');
 		return prompt;
 	}
 
 	if (num < 1) {
 		num = 1;
+	} else if (num > lineCount) {
+		num = lineCount;
 	}
 
-	string rawPrompt = "";
+	string rawPrompt = "initial";
 	int index = 0;
-	while (getline(inFS, rawPrompt)) {
+	while (index < num) {
+		getline(inFS, rawPrompt);
 		index++;
-		if (index == num) {
-			break;
-		}
 	}
+
+	inFS.close();
 
 	for (int i = 0; i < rawPrompt.size(); i++) {
 		prompt.push_back(rawPrompt.at(i));
